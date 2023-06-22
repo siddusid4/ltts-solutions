377984482309a7470881de58b79b4df20c652c23
siddusid4 <137388196+siddusid4@users.noreply.github.com>
2023-06-22 16:50:53 +0530

    Create cstoint.c

---
+++ Module1/Day5/cstoint.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100];
+    int i,num=0;
+    printf("enter string:");
+    scanf("%s",str);
+    for(i=0;i<strlen(str);i++){
+        num=num*10+(str[i]-48);
+    }
+    printf("the number is:%d",num);
+    return 0;
+}
