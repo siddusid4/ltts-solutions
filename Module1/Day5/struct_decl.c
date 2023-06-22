c6058dc29356a859eed6a1bf3f9c93bf70ccd1dc
siddusid4 <137388196+siddusid4@users.noreply.github.com>
2023-06-22 16:52:42 +0530

    Create struct_decl.c

---
+++ Module1/Day5/struct_decl.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define MAX_STUDENTS 30
+
+typedef struct {
+    char name[50];
+    int marks;
+} Student;
+void descending(Student students[], int numStudents) {
+    int i, j;
+    Student temp;
+    for (i = 0; i < numStudents - 1; i++) {
+        for (j = 0; j < numStudents - i - 1; j++) {
+            if (students[j].marks < students[j + 1].marks) {
+                temp = students[j];
+                students[j] = students[j + 1];
+                students[j + 1] = temp;
+            }
+        }
+    }
+}
+void main() {
+    Student students[MAX_STUDENTS];
+    int i, n;
+
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+
+    printf("Enter the student details:\n");
+    for (i = 0; i < n; ++i) {
+        printf("Enter name for student %d: ", i + 1);
+        scanf("%s", students[i].name);
+        printf("Enter marks for student %d: ", i + 1);
+        scanf("%d", &students[i].marks);
+    }
+    descending(students, n);
+    printf("The students in descending order are:\n");
+    for (i = 0; i < n; ++i) {
+        printf("Name: %s, Marks: %d\n", students[i].name, students[i].marks);
+    }
+}
