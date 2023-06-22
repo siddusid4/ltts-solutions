966632fe3efe353ba553ba466073c1f40bfea7e8
siddusid4 <137388196+siddusid4@users.noreply.github.com>
2023-06-22 16:46:07 +0530

    Create minmax1d.c

---
+++ Module1/Day4/minmax1d.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+void fun(int n,int arr[]);
+int main(){
+    int n,arr[50],i;
+    printf("enter n value:");
+    scanf("%d",&n);
+    printf("enter elements in array:");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    fun(n,arr);
+    return 0;
+}
+void fun(int n,int arr[]){
+    int i,j,temp;
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
