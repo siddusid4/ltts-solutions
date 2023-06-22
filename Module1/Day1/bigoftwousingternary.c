b11230b6aec98f8ae9b706c5c910db3045f7685a
siddusid4 <137388196+siddusid4@users.noreply.github.com>
2023-06-22 16:25:03 +0530

    Create bigoftwousingternary.c

---
+++ Module1/Day1/bigoftwousingternary.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int large_number(int num1, int num2)
+{
+    return (num1 > num2) ? num1 : num2;
+}
+
+int main() {
+    int num1, num2;
+    
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    int large = large_number(num1, num2);
+    
+    printf("The biggest number is: %d\n", large);
+    
+    return 0;
+}
